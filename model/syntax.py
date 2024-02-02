3eacd1c6af4df0f8675e67899f0f9c792992e210
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-02 22:31:22 +0530

    Fixing formula by using alpha as hyperparam

--- model/syntax.py
+++ model/syntax.py
@@ -8,15 +8,12 @@ def spelling_error(essay):
     spell_checker = SpellChecker("en_US")
     spell_checker.set_text(" ".join(words))
     total_misspelled_words = len([error.word for error in spell_checker])
-    print("Syntax errors identified: ")
-    for error in spell_checker:
-        print(error.word, error.suggestions)
-
-    return total_misspelled_words/math.sqrt(len(words))
+    return total_misspelled_words/len(words)
 
 def syntax_check(essay):
+    alpha = 1.15     # Penalty Factor
     syntax_ratio = spelling_error(essay)
-    syntax_penalty = syntax_ratio*10
+    syntax_penalty = alpha*syntax_ratio*10
     syntax_score = 10 - syntax_penalty
 
     return max(syntax_score, 0)
