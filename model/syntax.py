ecc19b59aabe6a01c0977e614f54528bef62f37a
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-03 15:22:18 +0530

    using enchant to recommend correct spellings

--- model/syntax.py
+++ model/syntax.py
@@ -1,13 +1,19 @@
 import nltk
 import math
 from nltk import word_tokenize
+import enchant
 from enchant.checker import SpellChecker
 
 def spelling_error(essay):
     words = word_tokenize(essay)
     spell_checker = SpellChecker("en_US")
+    sdict = enchant.Dict("en_US")
     spell_checker.set_text(" ".join(words))
     total_misspelled_words = len([error.word for error in spell_checker])
+    suggestions = []
+    for err in spell_checker:
+        suggestion += spell_checker.suggest(err.word)[0]
+        suggestions.append((err.word, suggestion))
     return total_misspelled_words/len(words)
 
 def syntax_check(essay):
