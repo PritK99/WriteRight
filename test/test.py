539e5feb1007176cea34021f90e436af290d786f
Prit Kanadiya <103832825+PritK99@users.noreply.github.com>
2024-02-03 21:24:01 +0530

    Adding code for evaluation using MSE

--- test/test.py
+++ test/test.py
@@ -1,6 +1,12 @@
 # Import necessary modules
 import os
 import pandas as pd
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.append(parent_dir)
+
 from model.statistics import statistics_check
 from model.syntax import syntax_check
 from model.semantic import semantic_check
@@ -25,7 +31,7 @@ def mean_squared_error(y, y_pred):
 
 prompt = "More and more people use computers, but not everyone agrees that this benefits society. Those who support advances in technology believe that computers have a positive effect on people. They teach hand-eye coordination, give people the ability to learn about faraway places and people, and even allow people to talk online with other people. Others have different ideas. Some experts are concerned that people are spending too much time on their computers and less time exercising, enjoying nature, and interacting with family and friends. Write a letter to your local newspaper in which you state your opinion on the effects computers have on people. Persuade the readers to agree with you."
 accuracy = 0
-iterations = 2
+iterations = 100
 
 # Testing over 100 examples
 for i in range (iterations):
@@ -33,7 +39,7 @@ for i in range (iterations):
     label = labels[i]
 
     # Hewlett dataset scores between 2-12
-    label = (label-2)*10/12
+    label = (label-2)
 
     suggestions = []
     statistic_score = statistics_check(essay, suggestions)
@@ -41,9 +47,10 @@ for i in range (iterations):
     semantic_score = semantic_check(prompt, essay, suggestions)
     total_score = (statistic_score + 2 * syntax_score + 2 * semantic_score) / 5
     error = mean_squared_error(label, total_score)
-    print(f"Error: {error:.1f}", end=" ")
-    print(f"Label: {label:.1f}", end=" ")
-    print(f"Total Score: {total_score:.1f}")
+    
+    print(f"iteration: {i:.1f}", end=" ")
+    print(f"Error: {error:.1f}")
+    
 
     accuracy += error
 
